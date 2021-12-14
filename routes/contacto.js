8e5faa45709c0621a4455e31a2b6824259d26b85
Diego Vottero Roca <diegovotteroroca@gmail.com>
2021-12-13 22:38:20 -0300

    boton activo + nodemailer + dotenv + funcinamiento del forulario

--- routes/contacto.js
+++ routes/contacto.js
@@ -1,8 +1,47 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 router.get('/', function (req,res,next) {
-    res.render('contacto') //contacto.hbs
+    res.render('contacto',{
+        isContacto: true
+    }) //contacto.hbs
 })
+
+/*funcionamiento del fotmulario*/
+router.post('/', async function (req,res,next){
+    //console.log(req.body);
+    var nombre = req.body.nombre;
+    var email = req.body.email;
+    var tel = req.body.tel;
+    var mensaje = req.body.comentarios;
+
+    var obj={
+        to: 'diego@gmail.com',
+        subjet: 'Contacto desde la web de transportes',
+        html: nombre + ' se contacto a traves de la web y quiere saber mas info a este correo: ' + email + '. <br> y su tel es ' + tel + '. <br> y su Comentario es: ' + mensaje + ' .'
+    }
+
+    var transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST, // escribir para que se comunique con .env
+        port: process.env.SMTP_PORT,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS    
+        }
+      });
+
+      var info = await transport.sendMail(obj); // envio de los datos
+
+      res.render('contacto',{
+          texto: 'Mensaje enviado correctamente',
+          isContacto: true
+      });
+
+});
+
+//async- await
+
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Diego' });
+  res.render('index', { 
+    isHome: true
+
+   });
 });
 
 module.exports = router;
